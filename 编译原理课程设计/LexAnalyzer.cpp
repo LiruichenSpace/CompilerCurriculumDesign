4384c68adf8c31774457485d29e8a63eb7eb9569
LiRuiChen <34571459+LiruichenSpace@users.noreply.github.com>
2020-04-27 18:51:08 +0800

    完善判断的边界条件

--- 编译原理课程设计/LexAnalyzer.cpp
+++ 编译原理课程设计/LexAnalyzer.cpp
@@ -16,9 +16,15 @@ LexAnalyzer::~LexAnalyzer()
 	deleteStream();
 }
 
+/**
+ * @brief 返回下一个Token，如果出错或者读到文件尾，返回的token类型为-1
+ * 注意，可能会无法区分文件读到头或者出现错误
+ */
 Token LexAnalyzer::getNextToken() {
 	//去除前导无效字符
 	trimStreamHead();
+	if (currChar==EOF)return Token();//如果已经读到了文件尾，则可以直接返回终结token
+	//处理输入文件可能为空或者滤去前导后为空
 	//DFA逻辑
 	Token token;
 	int currStatus = 0,nextStatus;
@@ -26,17 +32,17 @@ Token LexAnalyzer::getNextToken() {
 	bool flag=true;
 	while (flag) {
 		nextStatus = getDfaNextStatus(currStatus);//需要已经读入一个字符
-		if (nextStatus == -1) {//如果接下来没有转移去的状态，则结束
+		if (nextStatus == -1) {//如果接下来没有转移去的状态，则结束。不继续向下读，保护currChar
 			flag = false;
 		}
-		else {
+		else {//若被接受则去读下一个字符
 			str.push_back(currChar);//当前字符可被接受，继续进行
 			currStatus = nextStatus;
-		}
-		if(!istream.eof())istream >> currChar;//读取下一个字符
-		else {//文件结束,取消下一轮循环,此时应相当于Token加入了“#”，可以考虑直接返回特殊标志
-			flag = false;
-		}
+			currChar = istream.get();//读取下一个字符
+			if (currChar == EOF) {//文件结束,取消下一轮循环,此时应相当于Token加入了“#”，可以考虑直接返回特殊标志
+				flag = false;
+			}//由于EOF不会带来转移，到达EOF后重复调用也会获得无效Token
+		}	
 	}
 	if (isEndStatus(currStatus)) {//如果当前状态为终止状态，则填入token值并正常返回
 		token.strValue = str;
